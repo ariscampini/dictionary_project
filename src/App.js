dd7dd6362c80179752ee0fb5bf57c00e190fe258
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-04-15 20:09:49 +0200

    Added image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import image from './image.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Hello
+       
+
+       <img src={image} className="App-image img-fluid" alt="Image"/>  
       </header>
     </div>
   );
