67f32ed400775705db5dc5524c2430df8e11e596
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-04-16 21:00:18 +0200

    Added Phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Phonetic (props){
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio} target="blank">
+                Listen
+            </a>
+            <br/>
+            {props.phonetic.text}
+
+        </div>
+    )
+
+}
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning"
+import Phonetic from "./Phonetic"
 
 
 export default function Results (props) {
@@ -14,7 +15,15 @@ return (
         <h1>
          {props.results.word}
         </h1>
+        {props.results.phonetics.map(function(phonetic, index){
+            return (
+                <div key={index}>
+                <Phonetic phonetic={phonetic}  /> 
 
+                </div>
+            )
+
+        })}
         
         {props.results.meanings.map(function(meaning, index){
 
