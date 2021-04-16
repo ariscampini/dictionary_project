026e8331fa7b93c83824d09ba072027897c3d8ba
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-04-16 08:49:12 +0200

    API integration

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-       
-
-       <img src={image} className="App-image img-fluid" alt="cute monster"/>  
-      </header>
-      <Dictionary/>
-      <footer className="text-center">
-         This project was coded by Arianna Scampini and is {" "} 
-         <a className= "footer" rel="noreferrer"  href="https://github.com/ariscampini/react-weatherapp-finalproject" 
-      target="_blank">open source on GitHub</a>
-      </footer>
+        <header className="App-header">
+          <img src={image} className="App-image img-fluid" alt="cute monster"/>  
+        </header>
+          <Dictionary/>
+        <footer className="text-center">
+          This project was coded by Arianna Scampini and is {" "} 
+          <a className= "footer" rel="noreferrer"  href="https://github.com/ariscampini/react-weatherapp-finalproject" 
+          target="_blank">open source on GitHub</a>
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react"
+import React, {useState} from "react";
+import axios from "axios";
 import './Dictionary.css';
 
 
@@ -6,13 +7,22 @@ export default function Dictionary (){
 
     let [keyword, setKeyword] = useState (null)
 
+    function handleResponse (response){
+        console.log(response.data)
+    }
+
   
 
     function search (event){
         event.preventDefault();
-        alert (`Searching for ${keyword} definition`)
+       
 
+        let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+        axios.get(apiUrl).then(handleResponse);
     }
+
+
+
 
       function handleKeywordChange (event){
   
